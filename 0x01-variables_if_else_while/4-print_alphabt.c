74be128b6407b55696ddff13a39a286beec3acba
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-21 16:21:26 -0700

    c basics

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - entry point.
+* Return: Always 0
+*/
+
+int	main(void)
+{
+	char	cheta;
+	
+	for(cheta	=	'a';	cheta	<=	'z';	cheta++)
+	{
+		if((cheta	!=	'q')	&&	(cheta	!=	'e'))
+			putchar(cheta);
+	}
+
+	putchar('\n');
+	return (0);
+}
