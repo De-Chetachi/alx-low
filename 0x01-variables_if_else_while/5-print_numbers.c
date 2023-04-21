145c2ff453c324bfd1ac86190398a7718bb02faa
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-21 16:32:36 -0700

    c basics

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Result: Always 0
+*/
+
+int	main(void)
+{
+	int digi;
+
+	for	(digi	=	'0';	didi	<	'10';	digi++)
+	{
+		putchar(digi);
+	}
+
+	putchar('\n');
+	return (0);
+}
