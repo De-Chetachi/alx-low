0c30464685b5d7aa73e2e6dfeb4077c2c8343d1d
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-21 15:56:01 -0700

    c basics

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - entry point.
+* Return: Always 0
+*/
+
+int     main(void)
+{
+        char    cheta;
+
+	char	CHETA
+
+        for     (cheta  =       'a';    cheta   <=      'z';    cheta++)
+        {
+                putchar(cheta);
+        }
+	for     (CHETA  =       'A';    CHETA   <=      'Z';    CHETA++)
+	{
+		putchar(CHETA);
+	}
+        putchar('\n');
+
+        return (0);
+}
