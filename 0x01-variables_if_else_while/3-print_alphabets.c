7964c871d8a21ab4fd543126af343c266c07ddc0
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-21 16:04:53 -0700

    c basics

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,21 +6,21 @@
 
 int     main(void)
 {
-        char    cheta;
-
+	char    cheta;
+	
 	char	CHETA;
 
-        for     (cheta  =       'a';    cheta   <=      'z';    cheta++)
-        {
-                putchar(cheta);
-        }
-
+	for     (cheta  =       'a';    cheta   <=      'z';    cheta++)
+	{
+		putchar(cheta);
+	}
+	
 	for     (CHETA  =       'A';    CHETA   <=      'Z';    CHETA++)
 	{
 		putchar(CHETA);
 	}
-
-        putchar('\n');
-
-        return (0);
+	
+	putchar('\n');
+	
+	return (0);
 }
