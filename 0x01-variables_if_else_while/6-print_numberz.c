6dd0f165ac17854a5fa4348898ecc1a4d16ed962
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-21 17:15:59 -0700

    c basics

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,7 +7,7 @@ int	main(void)
 {
 	int	digi;
 
-	for	(digi	=	38;	digi	<=	48;	digi++)
+	for	(digi	=	48;	digi	<=	58;	digi++)
 	{
 		putchar(digi);
 	}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include	<stdio.h>
+/**
+* main - entry point
+* Return: Always 0
+*/
+
+int	main(void)
+{
+	int	digi;
+	char	cheta;
+
+	for	(digi	=	48;	digi	<	58;	digi++)
+		putchar(digi);
+	for	(cheta	=	'a';	cheta	<	'f';	cheta++)
+		putchar(cheta);
+
+	putchar('\n');
+	return	(0);
+}
