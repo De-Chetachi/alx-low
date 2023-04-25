1ed0717aee26155f7f7e042fd2e2f0f96dd0a112
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-25 14:25:39 -0700

    c functions

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include	<stdio.h>
+#include	"main.h"
+
+/**
+* print_sign(int n) - priints the alphabets in small letters
+* @n: The integer to be considered
+* Return: 0 If the argument is zero, 1 if the argument s positive, -1 if the argument is negative
+*/
+
+int	print_sign(int	n)
+{
+	if	(n	>	0)
+	{
+		putchar(43);
+		return	(1);
+	}
+	else
+	{
+		if	(n	<	0)
+		{
+			putchar(45);
+			return	(-1);
+		}
+		else
+		{
+			putchar(48);
+			return	(0);
+		}
+	}
+}
