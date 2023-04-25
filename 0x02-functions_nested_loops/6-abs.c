0ad9c40fe846771665c92bd31a56d65f429db8d3
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-25 16:48:22 -0700

    c functions

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 #include	"main.h"
 
 /**
-* int _abs - priints the alphabets in small letters
+* _abs - priints the alphabets in small letters
 * @p: The integer to be considered
 * Return: If the argument is lower 1, if the argument is not lover 0
 */
@@ -10,6 +10,7 @@
 int	_abs(int	p)
 {
 	int	result;
+
 	if	(p	>=	0)
 	{
 		result	=	p;
@@ -20,16 +21,16 @@ int	_abs(int	p)
 		int	temp;
 		int	sqrt;
 
-		q	=	p*p;
-		sqrt	=	q/2;
+		q	=	p	*	p;
+		sqrt	=	q	/	2;
 		temp	=	0;
 
 		while	(sqrt	!=	temp)
 		{
 			temp	=	sqrt;
-			sqrt	=	(q/temp	+temp)	/	2;
+			sqrt	=	((q	/	temp)	+	temp)	/	2;
 		}
 		result	=	sqrt;
 	}
-	return	result;
+	return	(result);
 }
