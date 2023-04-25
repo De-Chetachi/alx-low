d95aaf3d575c0f743ce524886b4306c717df588b
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-25 12:24:44 -0700

    c functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include	<stdio.h>
+
+/**
+* print_alphabet - priints the alphabets in small letters
+* Return: void
+*/
+
+void print_alphabet(void)
+{
+	char	a;
+
+	for	(a	=	97;	a	<	123;	a++)
+	{
+		putchar(a);
+	}
+	putchar('\n')
+	
+	return;
+}
+
+/**
+* main - entry point
+* Return: Always 0
+*/
+
+int	main
+{
+	print_alphabet();
+
+	return	(0);
+}
