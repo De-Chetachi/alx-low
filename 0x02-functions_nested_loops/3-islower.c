4a59a470002dec57c443f7715b73fe512b1d2c4d
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-25 13:50:52 -0700

    c functions

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include	<stdio.h>
+#include	"main.h"
+
+/**
+* _islower - priints the alphabets in small letters
+* Return: If the argument is lower 1, if the argument is not lover 0
+*/
+
+int	_islower(int	c)
+{
+	if	(c	>=	97	&&	c	<=	122)
+	{
+		return	(1);
+	}
+	else
+	{
+		return	(0);
+	}
+}
