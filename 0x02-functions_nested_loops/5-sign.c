e3e3b725174bc85d11288fa6da1e4815b9ba79f9
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-25 14:34:46 -0700

    c functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,19 +12,21 @@ int	print_sign(int	n)
 	if	(n	>	0)
 	{
 		putchar(43);
-		return	(1);
 	}
+	return	(1);
+
 	else
 	{
 		if	(n	<	0)
 		{
 			putchar(45);
-			return	(-1);
 		}
+		return	(-1);
+
 		else
 		{
 			putchar(48);
-			return	(0);
 		}
+		return	(0);
 	}
 }
