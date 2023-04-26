15f5542bddd1eaa08725e4d2ec2d30e7586b011a
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-25 17:59:31 -0700

    c functions

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,18 +3,27 @@
 #include	"main.h"
 
 /**
-* print_last_digit - priints the alphabets in small letters
+* print_last_digit - priints the last digit
 * @p: The integer to be considered
 * Return: The last digit
 */
 
 int	print_last_digit(int	p)
 {
-	int	result;
 	int	q;
 
 	q	=	p	%	10;
-	result	=	q;
 
-	return	(write(1,	q,	1));
+	if	(q	<	0)
+	{
+		_putchar(-q	+	48);
+		return	(-q);
+	}
+	else
+	{
+		_putchar(q	+	48);
+		return	(q);
+	}
+
+	return	();
 }
