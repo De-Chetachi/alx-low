758acfb2c4df8c9850a8deca2640ded36e103da4
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-25 18:17:31 -0700

    c functions

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,50 @@
+#include	<stdio.h>
+#include	<unistd.h>
+#include	"main.h"
+
+/**
+* jack_bauer - priints the last digit
+* Return: Nothing
+*/
+
+void	jack_bauer(void)
+{
+	int	a;
+	int	b;
+	int	c;
+	int	d;
+
+	for(a	=	0;	a	<=	2;	a++)
+	{
+		for(b	=	0;	b	<=	9;	b++)
+		{
+			for(c	=	0;	c	<=	5;	c++)
+			{
+				for(d	=	0;	d	<=	9;	d++)
+				{
+					if(a	>=	2	&&	b	>=	4)
+						break;
+					_putchar(a	+	48);
+					_putchar(b	+	48);
+					_putchar(48);
+					_putchar(c	+	48);
+					_putchar(d	+	48);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+
+	q	=	p	%	10;
+
+	if	(q	<	0)
+	{
+		_putchar(-q	+	48);
+		return	(-q);
+	}
+	else
+	{
+		_putchar(q	+	48);
+		return	(q);
+	}
+}
