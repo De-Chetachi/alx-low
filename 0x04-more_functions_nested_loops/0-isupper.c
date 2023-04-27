3fd980071d861d61b6a79cc8fd8fedf1a33c059f
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-27 12:32:45 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+* _isupper - checks if an input is an uppercase
+* @c: The integer to be considered
+* Return: 0 if c is uppercase, 1 otherwise
+*/
+
+int	_isupper(int	c)
+{
+	if	((c	<=	90)	&&	(c	>=	65))
+	{
+		return	(1);
+	}
+	else
+	{
+		return	(0);
+	}
+}
