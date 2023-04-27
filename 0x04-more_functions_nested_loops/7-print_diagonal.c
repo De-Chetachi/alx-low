dc01c1901638b2b3db051d320846d2c737cf0f41
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-27 16:09:46 -0700

    more fxns

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include	"main.h"
+
+/**
+* print_diagonal - prints a diagonal line
+* @n: An integer
+* Return: Null
+*/
+
+void	print_diagonal(int n)
+{
+	if	(n	<=	0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int	i;
+		int	j;
+
+		for	(i	=	0;	i	<	n;	i++)
+		{
+			for	(j	=	0;	j	<	n;	j++)
+			{
+				if	(j	==	i)
+				{
+					_putchar('\\');
+				}
+				else	if	(j	<	i)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
