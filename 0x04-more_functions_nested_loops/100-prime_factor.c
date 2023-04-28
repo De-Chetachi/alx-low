5e0990224636f608a3a74e3ae5b589bcd6a7687d
Onyenze Chetachi <chetagod4life@gmail.com>
2023-04-27 19:44:55 -0700

    more functions

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,8 +10,10 @@ int	main(void)
 {
 	long	i;
 	long	maxf;
+	long	number;
 	double	square;
 
+	number	=	612852475143;
 	square	=	sqrt(number);
 
 	for	(i	=	1;	i	<=	square;	i++)
@@ -22,4 +24,5 @@ int	main(void)
 		}
 	}
 	printf("%ld\n",	maxf);
+	return	(0);
 }
