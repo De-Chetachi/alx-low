fbb902d566a15d481f1d669e0d366ba5aefdd527
Onyenze Chetachi <chetagod4life@gmail.com>
2023-05-04 14:18:00 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two strings
+* @s1: Pointer to the first string
+* @s2: Pointer to the second string
+* Return: The difference
+*/
+
+int	_strcmp(char	*s1,	char	*s2)
+{
+	int	i;
+
+	for	(i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if	(s1[i] == s2[i])
+		{
+			i++;
+		}
+	}
+
+	return	(*s1 - *s2);
+}
