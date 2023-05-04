58170a509ab377799ae7e7bb542dcd7a3c602403
Onyenze Chetachi <chetagod4life@gmail.com>
2023-05-04 16:01:39 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include	"main.h"
+#include <ctype.h>
+
+/**
+* string_toupper - changes small letters to capital letter
+* @s: Pointer to the string to be modified
+* Return: Pointer to the modified string
+*/
+
+char	*string_toupper(char *s)
+{
+	int	i;
+
+	for	(i = 0; s[i] != '\0'; i++)
+	{
+		if	(islower(s[i]))
+		{
+			s[i]	= toupper(str[i]);
+		}
+	}
+	return	(s);
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* cap_string - capitalizes each word in a string
+* @s: The pointer to the string
+* Return: Pointer to the modified string
+*/
+
+char	*cap_string(char *s)
+{
+	int	i;
+
+	s[0]	=	toupper(s[0]);
+	for	(i = 1; s[i] != '\0'; i++)
+	{
+		if	(isspace(s[i - 1]) || ispunct(s[i - 1]))
+		{
+			s[i]	=	toupper(s[i]);
+		}
+	}
+}
