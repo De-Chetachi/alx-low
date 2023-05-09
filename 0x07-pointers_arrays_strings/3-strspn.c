e014e0c98d9fa895a05c2800c7b3fa9b5831f99f
Onyenze Chetachi <chetagod4life@gmail.com>
2023-05-08 17:04:35 -0700

    even more pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,8 +9,31 @@
 
 unsigned	int	_strspn(char *s, char *accept)
 {
-	int	i;
-	int	j;
-	int	n;
+	unsigned	int	i;
+	unsigned	int	j;
+	unsigned	int	n;
+	unsigned	int	m;
+
+	n	=	0;
+
+	for	(i = 0; s[i] != '\0'; i++)
+	{
+		m	=	0;
+
+		for	(j = 0; accept[j] != '\0'; j++)
+		{
+			if	(accept[j] == s[i])
+			{
+				n++;
+				m	=	1;
+			}
+		}
+		if	(m == 0)
+		{
+			return	(n);
+		}
+	
+	}
+	return	(n);
 }
 
