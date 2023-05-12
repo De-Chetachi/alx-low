63277356316e083c9b9ac75d517d28f3b258c707
Onyenze Chetachi <chetagod4life@gmail.com>
2023-05-12 16:12:20 -0700

    argc argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,18 +3,6 @@
 #include <stdio.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
 * main - prints the number of arguments passed into it
 * @argc: Number of arguments
 * @argv: Pointer to an array
