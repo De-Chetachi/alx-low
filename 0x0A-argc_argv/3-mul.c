456f597f0e5d68b5c631afcd3f4a2745f5b0ecb3
Onyenze Chetachi <chetagod4life@gmail.com>
2023-05-12 17:28:57 -0700

    products

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,6 +2,45 @@
 #include <unistd.h>
 #include <stdio.h>
 
+#include "main.h"
+/**
+* _atoi - converts a string to an integer
+* @s: The pointer to the string to be converted
+* Return: An integer
+*/
+
+int	_atoi(char *s)
+{
+	int	i,	uni,	sign;
+	unsigned int	num;
+
+	i	=	0;
+	uni	=	0;
+	sign	=	1;
+	num	=	0;
+
+	while	(s[i])
+	{
+		if	(s[i] == 45)
+		{
+			sign	=	sign	*	-1;
+		}
+		while	(s[i] >= 48 && s[i] <= 57)
+		{
+			uni	=	1;
+			num	=	(num * 10)	+	(s[i] - 48);
+			i++;
+		}
+		if	(uni == 1)
+		{
+			break;
+		}
+		i++;
+	}
+	num	=	num	*	sign;
+	return	(num);
+}
+
 /**
 * main - prints the product of two number
 * @argc: Number of arguments
@@ -14,8 +53,8 @@ int	main(int argc, char *argv[])
 	int	product;
 	int	a,	b;
 
-	a	=	*argv[1]	-	48;
-	b	=	*argv[2]	-	48;
+	a	=	_atoi(argv[1]);
+	b	=	_atoi(argv[2]);
 	if	(argc == 3)
 	{
 		product	=	a	*	b;
