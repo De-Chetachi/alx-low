01d6ceaebaf7a3734ae7cf3ad2f2f60110a32b12
Onyenze Chetachi <chetagod4life@gmail.com>
2023-05-16 14:55:58 -0700

    malloc argc argv

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+
+/**
+* argstostr -  concatenates all the arguments of your program
+* @ac: Number of arguments or height
+* @av: Pointer to a two dimensional array
+* Return: Returns a pointer to a new string, or NULL if it fails
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int	i,	j,	k,	l,	m;
+	int	sum;
+	char	*ptr;
+
+	if	(ac == 0 || av == NULL)
+		return	(NULL);
+	i	=	0;
+	j	=	0;
+	sum	=	0;
+
+	while	(i < ac)
+	{
+		while	(av[j])
+			j++;
+		sum	+=	j	+	1;
+		i++;
+	}
+	ptr	=	(char *)	malloc((sum * sizeof(char)) + 1);
+
+	if	(av == NULL || ac == 0)
+		return	(NULL);
+	k	=	0;
+	l	=	0;
+	m	=	0;
+	while	(av[k])
+	{
+		while	(av[k][l])
+		{
+			ptr[m]	=	av[k][l];
+			l++;
+			m++;
+		}
+		ptr[m]	=	'\n';
+		l	=	0;
+		k++;
+		m++;
+	}
+	ptr[m]	=	'\0';
+	return	(ptr);
+}
