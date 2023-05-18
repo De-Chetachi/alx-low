5e89ee13056a18e79bd187681571d7fa1daf4bef
Onyenze Chetachi <chetagod4life@gmail.com>
2023-05-18 13:26:52 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+
+/**
+* _calloc - returns a pointer to a 2 dimensional array of integers
+* @size: Length of the array
+* @nmemb: The number of arrays
+* Return: A pointer to the array, or NULL if it fails
+*/
+
+void	*_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned	int	i;
+	int	*ptr;
+
+	if	(nmemb <= 0 || size <= 0)
+		return	(NULL);
+
+	ptr	=	(int *)	malloc(sizeof(int) * size * nmemb);
+
+	if	(ptr == NULL)
+	{
+		return	(NULL);
+	}
+	for	(i = 0; i < nmemb; i++)
+	{
+		ptr[i]	=	0;
+	}
+	return	(ptr);
+}
