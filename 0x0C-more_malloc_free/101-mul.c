79206a9f504835d6f344aa665708f67bda194b6d
Onyenze Chetachi <chetagod4life@gmail.com>
2023-05-18 15:46:47 -0700

    mm

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+* main -  multiplies two positive numbers
+* Return: Void
+*/
+
+int	main(void)
+{
+	if	(argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return	(0);
+}
