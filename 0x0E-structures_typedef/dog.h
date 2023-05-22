7ee469055068da907665d226566745d30b925749
Onyenze Chetachi <chetagod4life@gmail.com>
2023-05-22 12:19:46 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,11 @@
 #define DOG_H
 
 /**
-* struct dog - defines a dog stucture
+* struct dog - Defines a dog stucture
 * @name: Dog name
 * @owner: Dog owner
 * @age: Dog age
+*
 * Description: Define a new type struct dog with the following elements
 */
 
@@ -18,9 +19,5 @@ struct	dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/**
- * dog_t *new_dog(char *name, float age, char *owner);
-*void free_dog(dog_t *d);
-*/
 
 #endif
