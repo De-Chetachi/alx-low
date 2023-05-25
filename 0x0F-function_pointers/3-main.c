d1c331395c7c804caef89bf5c9a89444254171e6
Onyenze Chetachi <chetagod4life@gmail.com>
2023-05-24 17:12:16 -0700

    fxn pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,30 +2,31 @@
 
 /**
 * main - entry point
+* @argc: Nomber og command line args
+* @argv: Pointr to an array of cmdline args
 * Return: An integer
 */
 
 int	main(int argc, char *argv[])
 {
 	int	a, b, result;
-	char	*s;
+	int	(*prt)(int, int);
 
 	a	=	atoi(argv[1]);
 	b	=	atoi(argv[3]);
-	s	=	argv[2];
-
 	if	(argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if	(!get_op_func(s))
+	prt	=	get_op_func(argv[2]);
+	if	(!prt)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	result  =       (*get_op_func(s))(a, b);
+	result  =       (*get_op_func(argv[2]))(a, b);
 	printf("%d\n", result);
 
 	return	(0);
