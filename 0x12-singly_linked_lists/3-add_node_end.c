2473a77544d3a4ec6d7aff15fd95cfb6634dee58
Chetachi Onyenze <chetaGod4life@gmail.com>
2023-06-01 14:36:56 -0700

    add node at end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -6,21 +6,21 @@
 * @str: Pointer to the node data
 * Return: Pointer to the new node
 */
-list_t	*add_node(list_t **head, const char *str)
+list_t	*add_node_end(list_t **head, const char *str)
 {
 	/*declare the new node*/
 	list_t	*node_end;
 	/*declare a temporary node to transverse the list*/
-	list_t	*node_temp;
+	list_t	*node_temp	=	*head;
 	int	i;
 	unsigned	int	length	=	0;
 
-	node_temp	=	*head;
 	/*allocate memory to the new node*/
 	node_end	=	malloc(sizeof(list_t));
 
 	if	(node_end == NULL)
 		return	(NULL);
+
 	node_end->str	=	strdup(str);
 
 	/*calculate the length of the string*/
@@ -28,7 +28,13 @@ list_t	*add_node(list_t **head, const char *str)
 		length++;
 
 	node_end->len	=	length;
+	node_end->next	=	NULL;
 
+	if	(*head == NULL)
+	{
+		*head	=	node_end;
+		return	(node_end);
+	}
 	/*transverse through the node till the node with null pointer*/
 	while	(node_temp->next != NULL)
 		node_temp	=	node_temp->next;
@@ -36,6 +42,6 @@ list_t	*add_node(list_t **head, const char *str)
 	/*change the null pointer to point to the new node*/
 	node_temp->next	=	node_end;
 
-	return	(*head);
+	return	(node_end);
 
 }
