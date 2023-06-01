2a191954c9ce0f3c7eeacbe5710891fcc450b8b5
Chetachi Onyenze <chetaGod4life@gmail.com>
2023-06-01 12:14:02 -0700

    print elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 /**
 * print_list - prints the elements of a list_t list
 * @h: Pointer to the head of the list
-* Reture: Nunmer of nodes
+* Return: Nunmer of nodes
 */
 size_t	print_list(const list_t *h)
 {
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,18 +6,18 @@
 #include <stddef.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
+* struct list_s - singly linked list
+* @str: String - (malloc'ed string)
+* @len: Length of the string
+* @next: Points to the next node
+*
+* Description: Singly linked list node structure
+*/
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
