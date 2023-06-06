0111f6d2c9389e49452eafdb60c3e7351be75230
Chetachi Onyenze <chetaGod4life@gmail.com>
2023-06-06 12:59:19 -0700

    add index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,37 +4,33 @@
 * @head: Pointer
 * @idx: Index
 * @n: Data
+* Return: Pointer to the new node, node_index
 */
 
 listint_t	*insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t	*prev,	*temp,	*node_index;
-	unsigned	int	length,	i	=	0;
+	listint_t	*node_temp,	*node_index;
+	unsigned	int	i	=	1;
 
-	length	=	0;
-
-	while	(*head != NULL)
-	{
-		*head	=	(*head)->next;
-		length	+=	1;
-	}
-
-	if	(idx > length)
-		return	(NULL);
-
-	temp	=	*head;
+	node_temp	=	*head;
 	node_index	=	malloc(sizeof(listint_t));
-	if	(node_index == NULL)
+
+	if	(node_index == NULL || head == NULL)
 		return	(NULL);
 	node_index->n	=	n;
-	while	(i < idx - 1)
+	if	(idx == 0)
 	{
-		prev	=	temp;
-		temp	=	temp->next;
-		i++;
+		node_index->next	=	*head;
+		*head	=	node_index;
+		return	(node_index);
 	}
 
-	prev->next	=	node_index;
-	node_index->next	=	temp;
+	while	(i < idx && node_temp !=NULL)
+	{
+		node_temp	=	node_temp->next;
+		i++;
+	}
+	node_index->next	=	node_temp->next;
+	node_temp->next	=	node_index;
 	return	(node_index);
 }
