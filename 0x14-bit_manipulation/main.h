960c74f01b73f9c9341aa9e764bfaeec312861f2
Chetachi Onyenze <chetaGod4life@gmail.com>
2023-06-08 15:29:50 -0700

    bitss

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,6 +9,11 @@
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
+int get_endianness(void);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int clear_bit(unsigned long int *n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index);
 void    print_rev(char  *s);
 
 #endif
