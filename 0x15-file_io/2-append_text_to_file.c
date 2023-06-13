f6dfc50b2f17ea357feb49b49e5a3ae6839edfbb
Chetachi Onyenze <chetaGod4life@gmail.com>
2023-06-13 14:22:11 -0700

    files

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* append_text_to_file - appends text at the end of a file.
+* @filename: Name of the file to be created
+* @text_content: A NULL terminated string t be appended in the file
+* Return: 1 or -1
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fdes;
+	int wrote;
+	int count = 0;
+
+	if (!filename)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[count])
+			count++;
+	}
+	fdes = open(filename, O_APPEND | O_WRONLY);
+	wrote = write(fdes, text_content, count);
+
+	if (fdes == -1 || wrote == -1)
+		return (-1);
+	close(fdes);
+	return (1);
+}
