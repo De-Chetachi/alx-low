371c9e525a04aff7371bf01b7fce3774de25d3cd
Chetachi Onyenze <chetaGod4life@gmail.com>
2023-06-13 14:01:18 -0700

    files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* read_textfile -  reads a text file and prints it to the sstandard output
+* @filename: Name of the file
+* @letters: Number of letters to read
+* Return: 0 or actual number of letters it could read and print
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t	fdes;
+	ssize_t sred;
+	ssize_t wrote;
+	char	*buffer;
+
+
+	fdes = open(filename, O_RDONLY);
+	if (fdes == 0)
+		return (-1);
+
+	buffer = malloc(sizeof(char) * letters);
+	sred = read(fdes, buffer, letters);
+	wrote = write(STDOUT_FILENO, buffer, sred);
+
+	free(buffer);
+	close(fdes);
+	return (wrote);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+file input and output, this project is about how to manipulate or work with files in our os
+operations like how to open a file, creat a file, write to a file or get text from a file etc
