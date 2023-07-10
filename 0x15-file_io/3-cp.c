5b446473837f168652ef1a8a819156fa0c8e4979
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-07-10 20:29:02 +0100

    :|

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * * _buffer - create a buffer of 1024 bits
+ * * @readfile: File whose buffers contents is to be stored in
+ * * Return: Pointer to the created beffer
+ * */
+char *_buffer(char *readfile)
+{
+	char *buff;
+
+
+	buff = malloc(sizeof(char) * 1024);
+
+
+	if (buff == NULL)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", readfile);
+		exit(99);
+	}
+
+
+	return (buff);
+}
+
+/**
+* _close - Closes the file with the spec file descriptors.
+* @fdes: The file descriptor of file  to be closed.
+* Return: Void
+*/
+void _close(int fdes)
+{
+	int cls;
+
+
+	cls = close(fdes);
+
+
+	if (cls == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdes);
+		exit(100);
+	}
+}
+
+
+/**
+ * * main - copies content from one file to anoyher
+ * * @ac: Number of command line args
+ * * @av: Commandline args
+ * * Return: Always 0.
+ * */
+int main(int ac, char **av)
+{
+	int strno;
+	int rdfrm;
+	int wrto;
+	int wrt;
+	char *buff;
+
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	buff = _buffer(av[2]);
+	rdfrm = open(av[1], O_RDONLY);
+	strno = read(rdfrm, buff, 1024);
+	wrto = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+
+	do {
+		if (rdfrm == -1 || strno == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			free(buff);
+			exit(98);
+		}
+		wrt = write(wrto, buff, strno);
+		if (wrto == -1 || wrt == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			free(buff);
+			exit(99);
+		}
+		strno = read(rdfrm, buff, 1024);
+		wrto = open(av[2], O_WRONLY | O_APPEND);
+	} while (strno > 0);
+
+
+	free(buff);
+	_close(rdfrm);
+	_close(wrto);
+	return (0);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,3 @@
+file input and output, this project is about how to manipulate or work with files in our os
+operations like how to open a file, creat a file, write to a file or get text from a file etc
+using system calls to perform some operations like opening, creating, writing to and reading from files
