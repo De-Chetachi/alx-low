ddfa44e3f175d35ab3ff544830425e45df9987d2
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-08-17 18:29:42 +0100

    print elements of dll

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* print_dlistint - prints all the elements of a dlistint_t list
+* @h: pointer to the first node of list
+* Return: the number of nodes
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	while (h->next)
+	{
+		printf("%d\n", h->n);
+		len++;
+		h = h->next;
+	}
+	printf("%d\n", h->n);
+	len++;
+
+	return (len);
+}
