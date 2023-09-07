fea5e2c4214fd80ec63ba7c5e237ae924d705aec
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-09-07 21:52:52 +0100

    a ton of debugging later

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,54 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
+	unsigned long int index, size;
+	hash_node_t *position, *head;
 
-	index = key_index((unsigned char))
+	if (strcmp(key, "") == 0)
+		return (0);
+	if (ht == NULL)
+		return (0);
+
+	size = ht->size;
+	index = key_index((unsigned char *)key, size);
+	position = ht->array[index];
+	head = node_add_begin(&position, (char *)key, (char *)value);
+	if (head == NULL)
+		return (0);
+	return (1);
+
+}
+
+/**
+* node_add_begin - adds a new node to the chain
+* @head: pointer to the head of the chain
+* @key: the key
+* @value: the value
+* Return: pointer to the modified head
+*/
+
+hash_node_t *node_add_begin(hash_node_t **head, char *key, char *value)
+{
+	hash_node_t *new_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = key;
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	return (*head);
+
 }
