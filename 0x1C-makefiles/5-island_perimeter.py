5ca986e08312be42e8a84f6dda71fc35fcc8c646
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-11-01 21:53:48 +0100

    island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''this module contains a function that finds the parameter
+of an island described by a grid'''
+
+
+def island_perimeter(grid):
+    '''perimeter of the island described in grid'''
+    param = 0
+    mat = len(grid)
+    list_ = len(grid[0])
+
+    for a in range(mat):
+        for b in range(list_):
+            if grid[a][b] == 1:
+                param = param + 4
+                if grid[a][b - 1] == 1:
+                    param -= 1
+                if grid[a][b + 1] == 1:
+                    param -= 1
+                if grid[a - 1][b] == 1:
+                    param -= 1
+                if grid[a + 1][b] == 1:
+                    param -= 1
+
+    return (param)
